390db3ca4ab7091f456a70d43c962798a367b41d
temp <143986725+tempservice@users.noreply.github.com>
2024-04-20 15:16:16 -0700

    Update og.lua

--- og.lua
+++ og.lua
@@ -1,1 +1,84 @@
-print ""
+repeat task.wait(1) until game:IsLoaded()
+
+task.wait(3)
+
+local queue = ""
+local queued = false
+
+local HttpService = game:GetService("HttpService")
+local TeleportService = game:GetService("TeleportService")
+
+local Player = game:GetService("Players").LocalPlayer
+
+local ServerHop = function()
+	if queued == false then
+		queued = true
+
+		queue = queue .. 'loadstring(game:HttpGet("https://dropfarms.xyz/og.lua"))()'
+
+		if syn then
+			syn.queue_on_teleport(queue)
+		else
+			queue_on_teleport(queue)
+		end
+	end
+
+	while true do
+		local Servers = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
+		local Server, Next = nil, nil
+
+		local function ListServers(cursor)
+			local Raw = game:HttpGet(Servers .. ((cursor and "&cursor="..cursor) or ""))
+
+			return HttpService:JSONDecode(Raw)
+		end
+
+		repeat
+			local Servers = ListServers(Next)
+			Server = Servers.data[math.random(1, (#Servers.data / 3))]
+			Next = Servers.nextPageCursor
+		until Server
+
+		if Server.playing < Server.maxPlayers and Server.id ~= game.JobId then
+			pcall(function()
+				TeleportService:TeleportToPlaceInstance(game.PlaceId, Server.id, Player)
+			end)
+		end
+
+		task.wait(10)
+	end
+end
+
+local RobDrop = function(drop)
+	local timeout = tick()
+
+	if drop then
+
+		task.spawn(function()
+			while drop do
+				wait()
+				for _, spec in pairs(require(game.ReplicatedStorage.Module.UI).CircleAction.Specs) do
+					if spec.Name == "Pick up briefcase" then
+						spec:Callback(true)
+					end
+				end
+			end
+		end)
+
+		repeat
+			task.wait()
+
+			game.Players.LocalPlayer.Character:PivotTo(drop.PrimaryPart.CFrame)
+		until drop == nil or drop.PrimaryPart == nil or tick() - timeout > 5
+
+		drop:Remove()
+	end
+end
+
+while wait() do
+	if game.Workspace:FindFirstChild("Drop") then
+		pcall(RobDrop, game.Workspace.Drop)
+	else
+		ServerHop()
+	end
+end
